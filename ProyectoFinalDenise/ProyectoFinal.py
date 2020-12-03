d9db2da814b8dbadc5a501ada5ad5bc01b4cc5f4
Denise Morales Escobedo <denisemorales8764@gmail.com>
2020-12-02 21:57:17 -0600

    Se agrego el metodo para saber si la palabra secreta ha sido adivinada o no, asi mismo se agrego un mini menu para saber si el usuario desea jugar de nuevo o no

--- ProyectoFinalDenise/ProyectoFinal.py
+++ ProyectoFinalDenise/ProyectoFinal.py
@@ -1,8 +1,20 @@
 import random
 
+class Oportunidades:
+    oportunidades = 0
+
+    def iniciarOportunidades(self):
+        self.oportunidades = 10
+
+    def reducirOp(self):
+        if(self.oportunidades > 0):
+            self.oportunidades -= 1
+
+    def getOportunidades(self):
+        return self.oportunidades
 
 class JuegoAhorcado:
-
+    op = Oportunidades()
 
     def cargarPalabras(self):
         cont = False
@@ -33,21 +45,45 @@ class JuegoAhorcado:
         abecedario = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
         letra = ""
         letrasIng = []
+        self.op.iniciarOportunidades()
 
 
 
-class Oportunidades:
-    oportunidades = 0
 
-    def iniciarOportunidades(self):
-        self.oportunidades = 10
+    def seAdivinoLaPalabra(self, palabra, letrasIng):
+        for letra in palabra.lower():
+            if(letra not in letrasIng):
+                while(self.op.getOportunidades() > 0):
+                    return False
 
-    def reducirOp(self):
-        if(self.oportunidades > 0):
-            self.oportunidades -= 1
+            if(self.op.getOportunidades() == 0):
+                print("No se ha adivinado la palabra :(")
+                print("La palabra secreta era: " + palabra)
+            else:
+                print("Felicidades has ganado el juego del Ahorcado! :)")
 
-    def getOportunidades(self):
-        return self.oportunidades
+            salida = False
+
+            while(salida != True):
+                print("Desea jugar de nuevo?")
+                print("1.- Si :)")
+                print("2.- No :(")
+                opcion = input()
+
+                if(opcion == "1" or opcion == "2"):
+                    salida = True
+                else:
+                    print("Opcion invalida, unicamente numeros del 1 al 2")
+                    salida == False
+
+            if(int(opcion) == 2):
+                print("Gracias por jugar! :)")
+                
+
+
+
+
+
 
 
 

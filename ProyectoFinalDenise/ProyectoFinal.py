7c82b64677231f554552ba58532eec3fe8dcad13
Denise Morales Escobedo <denisemorales8764@gmail.com>
2020-12-02 21:36:57 -0600

    Se creo ela clase oportunidades para poder ser utilizada en el metodo inicio ahorcado de la clase juego ahorcado

--- ProyectoFinalDenise/ProyectoFinal.py
+++ ProyectoFinalDenise/ProyectoFinal.py
@@ -25,6 +25,30 @@ class JuegoAhorcado:
     def elegirPalabra(self, listaPalab):
         return (listaPalab[int(random.randrange(len(listaPalab)))])
 
+    def inicioAhorcado(self, palabra):
+        print("--- Bienvenido al juego del Ahorcado ---")
+        print("Estoy pensando en una palabra de " + str(len(palabra)) + " letras")
+        print("¿Puedes adivinarla?")
+
+        abecedario = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+        letra = ""
+        letrasIng = []
+
+
+
+class Oportunidades:
+    oportunidades = 0
+
+    def iniciarOportunidades(self):
+        self.oportunidades = 10
+
+    def reducirOp(self):
+        if(self.oportunidades > 0):
+            self.oportunidades -= 1
+
+    def getOportunidades(self):
+        return self.oportunidades
+
 
 
 
